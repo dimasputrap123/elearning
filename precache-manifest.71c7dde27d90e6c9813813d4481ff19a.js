34386066ccc1275d47ffdcb6f974e7b928527190
fho123 <dimasp430@gmail.com>
2020-09-25 13:53:26 +0700

    Updates

---
+++ precache-manifest.71c7dde27d90e6c9813813d4481ff19a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b183bba1f9ffdea22a0e606a3a0a992c",
+    "url": "/elearning/index.html"
+  },
+  {
+    "revision": "5dfcef7b9a76163f241c",
+    "url": "/elearning/static/css/2.baa890a7.chunk.css"
+  },
+  {
+    "revision": "6bddac10db429a517cca",
+    "url": "/elearning/static/css/main.2d7d7860.chunk.css"
+  },
+  {
+    "revision": "5dfcef7b9a76163f241c",
+    "url": "/elearning/static/js/2.10eb178c.chunk.js"
+  },
+  {
+    "revision": "a0681c9baf682baa756ff9a27d770462",
+    "url": "/elearning/static/js/2.10eb178c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bddac10db429a517cca",
+    "url": "/elearning/static/js/main.10ab577b.chunk.js"
+  },
+  {
+    "revision": "8eea91f73b052066c5ca",
+    "url": "/elearning/static/js/runtime-main.b75fa590.js"
+  }
+]);
